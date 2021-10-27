ab2050c6b05c23996a2d6c11babadedbfe24bd12
Crys David <matrim.ivy@gmail.com>
2021-10-27 19:28:57 -0400

    finished with styling

--- BlogPlatform/wwwroot/js/site.js
+++ BlogPlatform/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener('DOMContentLoaded', function (open) {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+});
+
+
